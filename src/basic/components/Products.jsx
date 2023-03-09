3784db2a54cb7231b93896dcb97b403c907f7885
forhyeonji <ghkdguswl3@gmail.com>
2023-03-09 21:48:51 +0900

    loading, error 처리

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -4,28 +4,36 @@ export default function Products() {
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
   const handleChange = () => setChecked((prev) => !prev);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
+    setLoading(true);
+    setError(undefined);
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
         setProducts(data);
-      });
+      })
+      .catch((e) => setError('에러가 발생했음!'))
+      .finally(() => setLoading(false));
     return () => {
       console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
     };
   }, [checked]);
 
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
   return (
     <>
       <input
-        id='checkbox'
-        type='checkbox'
+        id="checkbox"
+        type="checkbox"
         value={checked}
         onChange={handleChange}
       />
-      <label htmlFor='checkbox'>Show Only 🔥 Sale</label>
+      <label htmlFor="checkbox">Show Only 🔥 Sale</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
